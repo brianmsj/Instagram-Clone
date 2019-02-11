757ee86fcb7b9137f4c23896ac1665b959bb6e2e
Brian McMinn (Contractor) <bmcminn@lmtc-bmcminn.attlocal.net>
2019-02-11 09:50:17 -0600

    first commit for this project

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    state = {
+      count: 10
+    }
+    constructor(props) {
+    super(props);
+     this.add = this.add.bind(this);
+     this.minus = this.minus.bind(this);
+}
+    add(){
+      console.log('adding')
+      this.setState({count: this.state.count + 1})
+    };
+    minus(){
+      console.log('minus')
+      this.setState({count: this.state.count - 1})
+    }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            How many apps can we build? {this.state.count}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={this.add}>add</button>
+          <button onClick={this.minus}>minus</button>
         </header>
       </div>
     );
